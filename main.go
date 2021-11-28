334ca77de049f6f6ea1a2fc16cb99fb617acff5f
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-28 18:47:14 +0600

    show image

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
@@ -13,14 +11,9 @@ func main() {
 	w := a.NewWindow("App title")
 	w.Resize(fyne.NewSize(400, 200))
 
-	// rgba color
-	colorx := color.NRGBA{
-		R: 0, G: 255, B: 0, A: 255,
-	}
+	img := canvas.NewImageFromFile("images/fyne.png")
 
-	textx := canvas.NewText("My text", colorx) //1st color, 2nd color
-
-	w.SetContent(textx)
+	w.SetContent(img)
 
 	w.ShowAndRun()
 }
