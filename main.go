3de5e8f2085269f7ea9dc6d4213aa0771cfa32a0
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-28 15:20:25 +0600

    button and checkbox

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
@@ -8,8 +10,24 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Raihan")
-	w.SetContent(widget.NewLabel("Hello world"))
+	w := a.NewWindow("Adb Desktop App")
+	// w.SetContent(widget.NewLabel("Hello world"))
 	w.Resize(fyne.NewSize(600, 300))
+	// btn := widget.NewButton("Show Devices", func() {
+	// 	commant, err := exec.Command("adb", "devices").Output()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Print(string(commant))
+
+	// })
+
+	check := widget.NewCheck("Check", func(b bool) {
+		fmt.Println(b)
+	})
+	// w.SetContent()
+
+	// w.SetContent(btn)
+	w.SetContent(check)
 	w.ShowAndRun()
 }
