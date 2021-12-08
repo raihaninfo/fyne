9d8f31fb3c482a7f252f2cbf4bcbab49fe054054
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-09 03:13:22 +0600

    random number genaretor

--- main.go
+++ main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
+	"math/rand"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Gradient")
-	// size
-	w.Resize(fyne.NewSize(500, 400))
+	w := a.NewWindow("Random Number Generator")
 
-	//
-	btn1 := widget.NewButton("click Me", func() {
+	// window size
+	w.Resize(fyne.NewSize(400, 400))
 
+	label1 := canvas.NewText("Rand Num Gen", color.White)
+	label1.TextSize = 40
+
+	btn1 := widget.NewButton("Generate", func() {
+		// logic
+		rand := rand.Intn(100)
+		label1.Text = fmt.Sprint(rand)
+		label1.Refresh()
 	})
-	label1 := widget.NewLabel("Here is my test")
 
-	box1 := container.NewHBox(
-		btn1,
-		label1,
-	)
-
+	// show
 	w.SetContent(
-		box1,
+		container.NewVBox(
+			label1,
+			btn1,
+		),
 	)
 
 	w.ShowAndRun()
