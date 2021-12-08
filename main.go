76aa77a693710006532263c507341330c7c4e224
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-09 02:46:53 +0600

    layout newhbox

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -14,9 +13,20 @@ func main() {
 	// size
 	w.Resize(fyne.NewSize(500, 400))
 
-	//gradient
-	gradient := canvas.NewLinearGradient(color.Black, color.White, 56)
-	w.SetContent(gradient)
+	//
+	btn1 := widget.NewButton("click Me", func() {
+
+	})
+	label1 := widget.NewLabel("Here is my test")
+
+	box1 := container.NewHBox(
+		btn1,
+		label1,
+	)
+
+	w.SetContent(
+		box1,
+	)
 
 	w.ShowAndRun()
 }
