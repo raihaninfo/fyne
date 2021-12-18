b1d023e1508c6746aa895147d9058fe045997d82
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-18 11:34:54 +0600

    create card

--- main.go
+++ main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Icon")
-	w.Resize(fyne.NewSize(500, 300))
+	w := a.NewWindow("CARD")
+	w.Resize(fyne.NewSize(600, 500))
 
-	icon := widget.NewIcon(theme.ConfirmIcon())
+	card := widget.NewCard(
+		"Hear is my card",
+		"Heare is my subTitle",
 
-	w.SetContent(icon)
+		canvas.NewImageFromFile("images/image1.jpg"),
+	)
 
+	w.SetContent(card)
 	w.ShowAndRun()
 }
