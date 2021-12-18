75648a5e0b7894519ef6c5ee1d01fc56154be4d9
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-18 14:34:27 +0600

    create internet information aplication

--- main.go
+++ main.go
@@ -1,24 +1,97 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+type IP struct {
+	Status   string
+	Country  string
+	Timezone string
+	Isp      string
+	Query    string
+}
+
+var ip IP
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("CARD")
+	w := a.NewWindow("What is my ip")
 	w.Resize(fyne.NewSize(600, 500))
 
-	card := widget.NewCard(
-		"Hear is my card",
-		"Heare is my subTitle",
+	status := widget.NewLabel("Status")
+	statusValue := widget.NewLabel("..........")
+	cuntry := widget.NewLabel("Cuntry")
+	cuntryValue := widget.NewLabel("..........")
+	timeZone := widget.NewLabel("Time Zone")
+	timeZoneValue := widget.NewLabel("..........")
+	isp := widget.NewLabel("Isp")
+	ispValue := widget.NewLabel("..........")
+	ip := widget.NewLabel("Internet ip")
+	ipValue := widget.NewLabel("..........")
+	btn := widget.NewButton("Run", func() {
+		status, cuntry, time, isp, ip := myIp()
+		statusValue.Text = status
+		cuntryValue.Text = cuntry
+		timeZoneValue.Text = time
+		ispValue.Text = isp
+		ipValue.Text = ip
+		statusValue.Refresh()
+		cuntryValue.Refresh()
+		timeZoneValue.Refresh()
+		ispValue.Refresh()
+		ipValue.Refresh()
+	})
 
-		canvas.NewImageFromFile("images/image1.jpg"),
+	w.SetContent(
+		container.NewVBox(
+			container.NewGridWithColumns(1,
+				container.NewGridWithColumns(2,
+					status,
+					statusValue,
+				),
+				container.NewGridWithColumns(2,
+					cuntry,
+					cuntryValue,
+				),
+				container.NewGridWithColumns(2,
+					timeZone,
+					timeZoneValue,
+				),
+				container.NewGridWithColumns(2,
+					isp,
+					ispValue,
+				),
+				container.NewGridWithColumns(2,
+					ip,
+					ipValue,
+				),
+				btn,
+			),
+		),
 	)
-
-	w.SetContent(card)
 	w.ShowAndRun()
+}
+
+func myIp() (string, string, string, string, string) {
+	req, err := http.Get("http://ip-api.com/json")
+	if err != nil {
+		panic(err)
+	}
+
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(body, &ip)
+	return ip.Status, ip.Country, ip.Timezone, ip.Isp, ip.Query
 }
