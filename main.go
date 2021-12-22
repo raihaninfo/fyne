0c0d36e7eb3b192676facb44ceb9e5782d4cb2b2
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-22 19:02:12 +0600

    load image from internet

--- main.go
+++ main.go
@@ -5,19 +5,21 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Custom app icon in title bar")
+	w := a.NewWindow("Loading image from internet")
 	w.Resize(fyne.NewSize(500, 400))
 
-	icon, err := fyne.LoadResourceFromPath("icon/app_icon.png")
+	img, err := fyne.LoadResourceFromURLString("https://picsum.photos/seed/picsum/200/300")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	// app icon
-	a.SetIcon(icon)
+	image := canvas.NewImageFromResource(img)
+	w.SetContent(image)
 
 	w.ShowAndRun()
 }
