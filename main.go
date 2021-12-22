616e196dbe38f7023adee30f69ca1aeb1767fda0
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-22 18:52:39 +0600

    fyne custom  app icon

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/theme"
 )
 
 func main() {
@@ -11,8 +12,12 @@ func main() {
 	w := a.NewWindow("Custom app icon in title bar")
 	w.Resize(fyne.NewSize(500, 400))
 
+	icon, err := fyne.LoadResourceFromPath("icon/app_icon.png")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	// app icon
-	w.SetIcon(theme.FyneLogo())
+	a.SetIcon(icon)
 
 	w.ShowAndRun()
 }
