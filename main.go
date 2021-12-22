5703bf05a84d79d2a142cfdd1df8b22f7951ca0d
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-22 18:40:37 +0600

    fyne app icon

--- main.go
+++ main.go
@@ -3,39 +3,16 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	// light
-	// a.Settings().SetTheme(theme.LightTheme())
-	a.Settings().SetTheme(theme.DarkTheme())
-	w := a.NewWindow("Dark and Light theme")
-
+	w := a.NewWindow("Custom app icon in title bar")
 	w.Resize(fyne.NewSize(500, 400))
 
-	label1 := widget.NewLabel("Fyne Themes")
-	btn1 := widget.NewButton("Light", func() {
-		a.Settings().SetTheme(theme.LightTheme())
-	})
-	btn2 := widget.NewButton("Dark", func() {
-		a.Settings().SetTheme(theme.DarkTheme())
-	})
-	btn3 := widget.NewButton("Exit", func() {
-		a.Quit()
-	})
-
-	w.SetContent(
-		container.NewVBox(
-			label1,
-			btn1,
-			btn2,
-			btn3,
-		),
-	)
+	// app icon
+	w.SetIcon(theme.FyneLogo())
 
 	w.ShowAndRun()
 }
