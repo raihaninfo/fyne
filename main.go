4f033db7e173c946861655916e8223decf463e1f
raihaninfo <raihanmahmudi35@gmail.com>
2021-12-23 19:28:48 +0600

    create main menu

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Loading image from internet")
+	w := a.NewWindow("Main Menu")
 	w.Resize(fyne.NewSize(500, 400))
-
-	img, err := fyne.LoadResourceFromURLString("https://picsum.photos/seed/picsum/200/300")
-	if err != nil {
-		fmt.Println(err.Error())
+	menuItem := &fyne.Menu{
+		Label: "File",
+		Items: nil,
 	}
-	// app icon
-	image := canvas.NewImageFromResource(img)
-	w.SetContent(image)
+	menu := fyne.NewMainMenu(menuItem)
+
+	w.SetMainMenu(menu)
 
 	w.ShowAndRun()
 }
