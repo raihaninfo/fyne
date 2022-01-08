f85dbf3ebd34f165b286bc1b76309f806df4452d
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-08 20:07:54 +0600

    logitn form

--- main.go
+++ main.go
@@ -1,65 +1,40 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Contact Form")
+	w := a.NewWindow("Login From")
+	a.Settings().SetTheme(theme.LightTheme())
+	// resize window
 	w.Resize(fyne.NewSize(500, 400))
-
-	title := canvas.NewText("Contact US", color.White)
-	title.TextSize = 20
-	title.TextStyle = fyne.TextStyle{Bold: true}
-	title.Resize(fyne.NewSize(300, 35))
-	title.Move(fyne.NewPos(50, 10))
-
-	// new field name
-	entry_name := widget.NewEntry()
-	entry_name.SetPlaceHolder("Enter Your name")
-	entry_name.Resize(fyne.NewSize(300, 35))
-	entry_name.Move(fyne.NewPos(50, 50))
-
-	// new field Email
-	entry_email := widget.NewEntry()
-	entry_email.SetPlaceHolder("Enter Your Email")
-	entry_email.Resize(fyne.NewSize(300, 35))
-	entry_email.Move(fyne.NewPos(50, 100))
-
-	// new field Email
-	entry_address := widget.NewEntry()
-	entry_address.SetPlaceHolder("Enter Your Address")
-	entry_address.Resize(fyne.NewSize(300, 35))
-	entry_address.Move(fyne.NewPos(50, 150))
-
-	// new field Email
-	entry_message := widget.NewEntry()
-	entry_message.SetPlaceHolder("Enter Your Message")
-	entry_message.MultiLine = true
-	entry_message.Resize(fyne.NewSize(300, 150))
-	entry_message.Move(fyne.NewPos(50, 200))
-
-	// submit btn
-	submit_btn := widget.NewButton("Submit", nil)
-	submit_btn.Resize(fyne.NewSize(80, 30))
-	submit_btn.Move(fyne.NewPos(50, 355))
+	label := widget.NewLabel("")
+	form := widget.NewForm(
+		widget.NewFormItem("Username", widget.NewEntry()),
+		widget.NewFormItem("Password", widget.NewPasswordEntry()),
+	)
+	// working and cancel
+	form.OnCancel = func() {
+		label.Text = "Canceled"
+		label.Refresh()
+	}
+	form.OnSubmit = func() {
+		label.Text = "Submited"
+		label.Refresh()
+	}
 
 	w.SetContent(
-		container.NewWithoutLayout(
-			title,
-			entry_name,
-			entry_email,
-			entry_address,
-			entry_message,
-			submit_btn,
+		container.NewVBox(
+			form,
+			label,
 		),
 	)
+
 	w.ShowAndRun()
 }
